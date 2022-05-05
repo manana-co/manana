5a39b5621092220411c07e3decf4c40a25019774
Scott Cameron <mscottcam@gmail.com>
2022-05-04 22:04:46 -0500

    wraps landing image to crop top

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,34 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { Box, Image } from '@chakra-ui/react'
+import { Box, Image, useTheme } from '@chakra-ui/react'
 import { TopNavbar } from 'components/top-navbar'
 
 const Home: NextPage = () => {
+  const {
+    colors: { brandBlue },
+  } = useTheme()
   return (
-    <div>
+    <Box bg={brandBlue}>
       <Head>
         <title>Mañana Co.</title>
       </Head>
       <TopNavbar />
-      <Image
-        src="main-landing-defender.jpeg"
-        alt="main landing image"
-        width="100vw"
-      />
+      <Box
+        maxHeight="100vh"
+        overflow="hidden"
+        display="flex"
+        alignItems="center"
+      >
+        <Image
+          src="main-landing-defender.jpeg"
+          alt="main landing image"
+          width="100vw"
+        />
+      </Box>
       <main>
         <Box height="12000px" />
       </main>
-      <footer></footer>
-    </div>
+    </Box>
   )
 }
 
