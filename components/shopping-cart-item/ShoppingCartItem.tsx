d45aaed93d9173cffb297567cc67ca76dd730f52
Scott Cameron <mscottcam@gmail.com>
2022-05-23 13:55:51 -0500

    shopping cart header / footer

---
+++ components/shopping-cart-item/ShoppingCartItem.tsx
@@ -0,0 +1,21 @@
+import { Grid, Box, Stack, Text, Select } from '@chakra-ui/react'
+function ShoppingCartItem() {
+  // const quantity = 2
+  // const dropDownItems = Array.from(new Array(quantity + 2), x => x)
+
+  return (
+    <Grid height="5rem" templateColumns="3" templateRows="1">
+      {/* <Box height="100%" border="1px solid red" />
+      <Stack>
+        <Text>Molo</Text>
+        <Text>{`Tan, 6' 0"`}</Text>
+      </Stack>
+      <Stack direction="column">
+        <Text>$899</Text>
+        <Text>Remove</Text>
+      </Stack> */}
+    </Grid>
+  )
+}
+
+export { ShoppingCartItem }
--- components/shopping-cart/ShoppingCart.tsx
+++ components/shopping-cart/ShoppingCart.tsx
@@ -1,18 +1,51 @@
-import { Drawer, DrawerBody, DrawerOverlay, DrawerContent, useTheme } from '@chakra-ui/react'
+import {
+  Drawer,
+  DrawerBody,
+  DrawerOverlay,
+  DrawerContent,
+  useTheme,
+  Heading,
+  DrawerFooter,
+  Text,
+  Button,
+  Flex,
+  Stack,
+  DrawerHeader,
+} from '@chakra-ui/react'
 
 import { CloseButton } from 'components/close-button'
+import { ShoppingCartItem } from 'components/shopping-cart-item'
 
 function ShoppingCart({ isOpen, onClose }: Props) {
   const {
-    colors: { brandBlue },
+    colors: { brandBlue, brandWhite },
   } = useTheme()
+
+  const numberOfCartItems = 2
 
   return (
     <Drawer onClose={onClose} isOpen={isOpen} size="md" placement="right">
       <DrawerOverlay />
-      <DrawerContent bg={brandBlue}>
+      <DrawerContent bg={brandBlue} color={brandWhite}>
         <CloseButton onClick={onClose} />
-        <DrawerBody paddingTop="2rem">Hello</DrawerBody>
+        <DrawerHeader>
+          <Heading size="lg">Shopping Cart ({`${numberOfCartItems}`} items)</Heading>
+        </DrawerHeader>
+        <DrawerBody padding="2rem">
+          <ShoppingCartItem />
+        </DrawerBody>
+        <DrawerFooter>
+          <Stack direction="column" width="100%">
+            <Flex justifyContent="space-between">
+              <Heading size="lg">Subtotal:</Heading>
+              <Heading size="lg">{`$${899.0}`}</Heading>
+            </Flex>
+            <Text opacity={0.7} pb="1rem">
+              Shipping and taxes calculated at checkout
+            </Text>
+            <Button color="#000000">Checkout</Button>
+          </Stack>
+        </DrawerFooter>
       </DrawerContent>
     </Drawer>
   )
