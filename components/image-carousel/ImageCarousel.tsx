d4a4265599f4fb140d4068f6f361ea8b6ed72a57
Scott Cameron <mscottcam@gmail.com>
2022-06-03 17:09:01 -0500

    basic carousel working

--- components/image-carousel/ImageCarousel.tsx
+++ components/image-carousel/ImageCarousel.tsx
@@ -1,27 +1,50 @@
 import Image from 'next/image'
-import { Box, useTheme, Stack } from '@chakra-ui/react'
-import { ProductImage } from 'components/product-image'
+import { Flex, Box, Button } from '@chakra-ui/react'
+// import { ProductImage } from 'components/product-image'
+
+const mockImageSrcs = [
+  '/hat-example.png',
+  '/board-example.png',
+  '/mug-example.png',
+  '/shirt-example.png',
+  '/shorts-example.png',
+]
 
 function ImageCarousel() {
-  const {
-    colors: { brandTan },
-  } = useTheme()
   return (
-    <Box bg={brandTan} height="100%" width="100%">
-      <Stack
-        direction="row"
-        spacing="2rem"
-        padding="2rem"
-        overflowX="auto"
-        height="100%"
-        border="1px solid red"
-      >
-        <Image layout="fill" src="/hat-example.png" alt="test" />
-        <Image layout="fill" src="/board-example.png" alt="test" />
-        <Image layout="fill" src="/mug-example.png" alt="test" />
-        <Image layout="fill" src="/shirt-example.png" alt="test" />
-        <Image layout="fill" src="/shorts-example.png" alt="test" />
-      </Stack>
+    <Box bg="transparent" height="100%" width="100%">
+      <Flex overflowX="auto" scrollSnapType="x mandatory" scrollBehavior="smooth" height="100%">
+        {mockImageSrcs.map((src) => (
+          <Box
+            key={src}
+            id={src}
+            scrollSnapAlign="start"
+            flexShrink={0}
+            marginRight="50px"
+            height="100%"
+            width="100%"
+            borderRadius="10px"
+            transformOrigin="center center"
+            transform="scale(1)"
+            transition="transform 0.5s"
+            position="relative"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Image src={src} alt="test" height={200} width={200} />
+          </Box>
+        ))}
+      </Flex>
+      {/* Buttons <a href="#picture-1" /> */}
+      {mockImageSrcs.map((src) => (
+        <Button
+          as="a"
+          key={src}
+          href={`#${src}`}
+          style={{ width: '10px', height: '10px' }}
+        ></Button>
+      ))}
     </Box>
   )
 }
