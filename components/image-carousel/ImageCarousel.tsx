5a1bc40638351e24cec127329778b91e0bda3b53
Scott Cameron <mscottcam@gmail.com>
2022-06-13 21:10:10 -0500

    carousel buttons working properly

--- components/image-carousel/ImageCarousel.tsx
+++ components/image-carousel/ImageCarousel.tsx
@@ -1,5 +1,6 @@
+import { useState } from 'react'
 import Image from 'next/image'
-import { Flex, Box, Button } from '@chakra-ui/react'
+import { Flex, Box, Button, useTheme } from '@chakra-ui/react'
 
 import hatImage from 'public/hat-example.png'
 import boardImage from 'public/board-example.png'
@@ -16,6 +17,11 @@ const mockImageSrcs = [
 ]
 
 function ImageCarousel() {
+  const {
+    colors: { brandBlue },
+  } = useTheme()
+  const [activePicture, setActivePicture] = useState(0)
+
   return (
     <Box bg="transparent" height="100%" width="100%">
       <Flex
@@ -24,24 +30,42 @@ function ImageCarousel() {
         scrollBehavior="smooth"
         height="100%"
         position="relative"
+        sx={{
+          scrollbarWidth: 'none',
+          '::-webkit-scrollbar': {
+            display: 'none',
+          },
+          '-ms-overflow-style': 'none',
+        }}
       >
         {mockImageSrcs.map(({ src, id }) => (
           <Box key={id} id={id} flexShrink={0} height="100%" width="100%" position="relative">
-            <Image src={src} alt="test" layout="fill" objectFit="contain" />
+            <Image src={src} alt="test" layout="fill" objectFit="contain" priority />
           </Box>
         ))}
       </Flex>
-      {mockImageSrcs.map(({ id }) => (
-        <Button
-          as="a"
-          key={id}
-          onClick={(e) => {
-            e.preventDefault()
-            const photo = document.getElementById(id)
-            photo?.scrollIntoView({ block: 'nearest' })
-          }}
-        ></Button>
-      ))}
+      <Flex justifyContent="center">
+        {mockImageSrcs.map(({ id }, index) => (
+          <Button
+            key={id}
+            onClick={(e) => {
+              e.preventDefault()
+              const photo = document.getElementById(id)
+              setActivePicture(index)
+              photo?.scrollIntoView({ block: 'nearest' })
+            }}
+            background={activePicture === index ? brandBlue : 'none'}
+            variant="unstyled"
+            border={`1px solid ${brandBlue}`}
+            height="1rem"
+            width="1rem"
+            minWidth="1rem"
+            borderRadius="10px"
+            margin="0 0.5rem"
+            _focus={{ boxShadow: 'none' }}
+          ></Button>
+        ))}
+      </Flex>
     </Box>
   )
 }
