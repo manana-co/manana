0d601377cf995d0b361af3665e496dc60ab767d5
Scott Cameron <mscottcam@gmail.com>
2022-06-26 17:12:23 -0500

    smaller splash image for mobile, removes scrollbars entirely

--- components/splash-image/SplashImage.tsx
+++ components/splash-image/SplashImage.tsx
@@ -1,10 +1,15 @@
 import Image, { StaticImageData } from 'next/image'
-import { AspectRatio } from '@chakra-ui/react'
+import { AspectRatio, useMediaQuery } from '@chakra-ui/react'
 
 import { useAspectRatio } from 'hooks/useAspectRatio'
 
 function SplashImage({ image, imagePosition = 'center' }: Props) {
   const [width, height] = useAspectRatio()
+  const [isSmallerThan400] = useMediaQuery('(max-width: 500px)')
+
+  if (isSmallerThan400) {
+    return <Image src={image} alt="main landing image" layout="responsive" quality={100} priority />
+  }
 
   return (
     <AspectRatio position="relative" ratio={width / height}>
