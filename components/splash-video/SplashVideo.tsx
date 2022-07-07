fbf3a4ed982d5e9adadcc8b0697e499ac2b0f345
Scott Cameron <mscottcam@gmail.com>
2022-07-07 13:57:11 -0500

    allowing autoplay vid on mobile

--- components/splash-video/SplashVideo.tsx
+++ components/splash-video/SplashVideo.tsx
@@ -3,7 +3,7 @@ import { AspectRatio } from '@chakra-ui/react'
 function SplashVideo() {
   return (
     <AspectRatio ratio={16 / 9}>
-      <video autoPlay loop muted>
+      <video autoPlay loop muted playsInline onContextMenu={() => false} preload="auto">
         <source src="main_vid.mp4" type="video/mp4" />
       </video>
     </AspectRatio>
